e17539a7af0e12c65da8131a9a04fef1c16ea0ca
arun4033622@gmail.com <arun4033622@gmail.com>
2020-03-08 21:15:43 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  subdomain: "api",
+  consumer_key: "YOUR_CONSUMER_KEY",
+  consumer_secret: "YOUR_CONSUMER_SECRET",
+  access_token_key: "YOUR_ACCESS_TOKEN_KEY",
+  access_token_secret: "YOUR_ACCESS_TOKEN_SECRET"
+};
---
+++ motivationalReplies.js
@@ -0,0 +1,12 @@
+module.exports = [
+  "Keep coding ###",
+  "Happy coding ###! :)",
+  "Never stop learning ###!",
+  "Keep making progress ###. Little by little becomes a lot! :)",
+  "\"Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.\" - Patrick McKenzie, Keep learning ###!",
+  "Go go go ###! Always be coding :D",
+  "\"Those hours of practice, and failure, are a necessary part of the learning process.\"- Gina Sipley, Keep going ###!",
+  "Conquer the world ###!",
+  "It's ### time!",
+  "You got this ### :)"
+]
