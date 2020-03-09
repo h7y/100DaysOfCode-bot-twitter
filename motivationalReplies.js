61127cec3da3fe827148c56a4dbab10618b070ae
arun4033622@gmail.com <arun4033622@gmail.com>
2020-03-09 21:10:18 +0530

    Add more replies 💪🏽

--- motivationalReplies.js
+++ motivationalReplies.js
@@ -1,5 +1,5 @@
 module.exports = [
-  "Keep coding ###",
+  "Keep coding ###! :D",
   "Happy coding ###! :)",
   "Never stop learning ###!",
   "Keep making progress ###. Little by little becomes a lot! :)",
@@ -8,5 +8,10 @@ module.exports = [
   "\"Those hours of practice, and failure, are a necessary part of the learning process.\"- Gina Sipley, Keep going ###!",
   "Conquer the world ###!",
   "It's ### time!",
-  "You got this ### :)"
+  "You got this ### :)",
+  "Fall seven times, stand up eight. – Unknown, you got this ###!",
+  "All the power is within you. You can do anything ###!",
+  "Be so good they can’t ignore you ###.",
+  "The habit of persistence is the habit of victory. - Herbert Kaufman, Happy coding ###! :)",
+  "Prove them wrong ###!"
 ]
